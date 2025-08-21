05e1fcd065c78c0afa912035bd6b71052e2356a3
Thế Anh Đỗ <theanhdo@live.com>
2025-08-21 23:00:03 +0700

    Bài 18

--- iCons/Models/mUser.cs
+++ iCons/Models/mUser.cs
@@ -15,8 +15,17 @@ namespace iCons.Models
         [Test]
         public static async Task Test()
         {
-            var kq = await GetUserByEmail("anhdt@xuanmaicorp.vn");
-            Console.WriteLine(kq.Message);
+            for (var i=1; i < 10; i++)
+            {
+                var kq = await AddUser(new efUser
+                {
+                    UserName = "testuser" + i,
+                    Email = "testemail"+ i +"@gmail.com",
+                    FullName = "Test User "+ i,
+                });
+                Console.WriteLine(kq.Message);
+            }
+            
         }
 
 
@@ -38,6 +47,8 @@ namespace iCons.Models
                             if (uList!=null)
                             {
                                 userList = uList;
+                                isSuccess = true;
+                                message = "Đã tải danh sách người dùng";
                             }    
                         }
                         else
@@ -277,6 +288,188 @@ namespace iCons.Models
             return (isSuccess, message);
         }
 
+        public static async Task<(bool IsSuccess, string Message)> AddUser(efUser UserInfo)
+        {
+            bool isSuccess = false;
+            string message = string.Empty;
+            efUser userInfo = new efUser();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(App.ConnectionString))
+                {
+                    using (var _dbContext = new AppDbContext(App.ConnectionString))
+                    {
+                        if (_dbContext != null)
+                        {
+                            var uInfo = await _dbContext.Users
+                                .Where(o => o.UserName == UserInfo.UserName || o.Email == UserInfo.Email)
+                                .FirstOrDefaultAsync();
+
+
+                            if (uInfo == null)
+                            {
+                                _dbContext.Users.Add(UserInfo);
+                                var kqChange = await _dbContext.SaveChangesAsync();
+
+                                if (kqChange > 0)
+                                {
+                                    isSuccess = true;
+                                    message = "Đã thêm người dùng";
+
+
+                                }
+                                else
+                                {
+                                    message = "Không thể thên người dùng";
+                                }
+                            }
+                            else
+                            {
+                                message = "Username hoặc Email đã tồn tại";
+                            }
+                        }
+                        else
+                        {
+                            message = "Không thể kết nối đến cơ sở dữ liệu";
+                        }
+                    };
+                }
+                else
+
+                {
+                    message = "Không có chuỗi kết nối";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return (isSuccess, message);
+        }
+
+        public static async Task<(bool IsSuccess, string Message)> DeleteUser(efUser UserInfo)
+        {
+            bool isSuccess = false;
+            string message = string.Empty;
+            efUser userInfo = new efUser();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(App.ConnectionString))
+                {
+                    using (var _dbContext = new AppDbContext(App.ConnectionString))
+                    {
+                        if (_dbContext != null)
+                        {
+                            var uInfo = await _dbContext.Users
+                                .Where(o => o.UserName == UserInfo.UserName)
+                                .FirstOrDefaultAsync();
+
+
+                            if (uInfo != null)
+                            {
+                                _dbContext.Users.Remove(uInfo);
+                                var kqChange = await _dbContext.SaveChangesAsync();
+
+                                if (kqChange > 0)
+                                {
+                                    isSuccess = true;
+                                    message = "Đã xóa người dùng";
+
+
+                                }
+                                else
+                                {
+                                    message = "Không thể xóa người dùng";
+                                }
+                            }
+                            else
+                            {
+                                message = "Username không tồn tại";
+                            }
+                        }
+                        else
+                        {
+                            message = "Không thể kết nối đến cơ sở dữ liệu";
+                        }
+                    };
+                }
+                else
+
+                {
+                    message = "Không có chuỗi kết nối";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return (isSuccess, message);
+        }
+
+        public static async Task<(bool IsSuccess, string Message)> DeleteUserList(ICollection<efUser> UserList)
+        {
+            bool isSuccess = false;
+            string message = string.Empty;
+            efUser userInfo = new efUser();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(App.ConnectionString))
+                {
+                    using (var _dbContext = new AppDbContext(App.ConnectionString))
+                    {
+                        if (_dbContext != null)
+                        {
+                            var UNameList = UserList.Select(o => o.UserName).ToList();
+
+
+                            var uInfoList = await _dbContext.Users
+                                .Where(o => UNameList.Contains(o.UserName))
+                                .ToListAsync();
+
+
+                            if (uInfoList != null && uInfoList.Count >0)
+                            {
+                                _dbContext.Users.RemoveRange(uInfoList);
+                                var kqChange = await _dbContext.SaveChangesAsync();
+
+                                if (kqChange > 0)
+                                {
+                                    isSuccess = true;
+                                    message = $"Đã xóa {kqChange} người dùng";
+
+
+                                }
+                                else
+                                {
+                                    message = "Không thể xóa người dùng";
+                                }
+                            }
+                            else
+                            {
+                                message = "Username không tồn tại";
+                            }
+                        }
+                        else
+                        {
+                            message = "Không thể kết nối đến cơ sở dữ liệu";
+                        }
+                    };
+                }
+                else
+
+                {
+                    message = "Không có chuỗi kết nối";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return (isSuccess, message);
+        }
 
     }
 }
---
+++ iCons/ViewModels/Main/vmAdmin.cs
@@ -0,0 +1,156 @@
+﻿using iCons.Classes;
+using iCons.Models;
+using K010Libs.Mvvm;
+using Microsoft.Xaml.Behaviors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace iCons.ViewModels.Main
+{
+    public class vmAdmin : PropertyChangedBase
+    {
+        private ObservableRangeCollection<efUser> _UserList = new ObservableRangeCollection<efUser>();
+        public ObservableRangeCollection<efUser> UserList
+        {
+            get
+            {
+                return _UserList;
+            }
+            set
+            {
+                _UserList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableRangeCollection<efUser> _UserSelect = new ObservableRangeCollection<efUser>();
+        public ObservableRangeCollection<efUser> UserSelect
+        {
+            get
+            {
+                return _UserSelect;
+            }
+            set
+            {
+                _UserSelect = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
+
+        private ActionCommand loadAll;
+
+        public ICommand LoadAll
+        {
+            get
+            {
+                if (loadAll == null)
+                {
+                    loadAll = new ActionCommand(PerformLoadAll);
+                }
+
+                return loadAll;
+            }
+        }
+
+        private async void PerformLoadAll()
+        {
+            var vmMainDc = App.Current.MainWindow.DataContext as vmMain;
+
+            string Message = string.Empty;
+
+            try
+            {
+                UserList.Clear();
+
+                vmMainDc.IsWinActive = false;
+                vmMainDc.PbIsIndeterminate = true;
+                vmMainDc.PbMessage = "Đang tải thông tin...";
+
+                var kq = await Task.Run(async () =>
+                {
+                    return await mUser.GetAllUsers();
+                });
+
+                Message = kq.Message;
+
+                if (kq.IsSuccess)
+                {
+                    UserList.AddRange(kq.UserList);
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+            finally
+            {
+                vmMainDc.IsWinActive = true;
+                vmMainDc.SbMessage?.Enqueue(Message, null, null, null, false, true, TimeSpan.FromSeconds(2));
+            }
+        }
+
+        private ActionCommand deleteUser_Command;
+
+        public ICommand DeleteUser_Command
+        {
+            get
+            {
+                if (deleteUser_Command == null)
+                {
+                    deleteUser_Command = new ActionCommand(DeleteUser_);
+                }
+
+                return deleteUser_Command;
+            }
+        }
+
+        private async void DeleteUser_()
+        {
+            var vmMainDc = App.Current.MainWindow.DataContext as vmMain;
+
+            string Message = string.Empty;
+
+            try
+            {
+                if (UserSelect!=null && UserSelect.Count>0)
+                {
+                    vmMainDc.IsWinActive = false;
+                    vmMainDc.PbIsIndeterminate = true;
+                    vmMainDc.PbMessage = "Đang tải thông tin...";
+
+                    var kq = await Task.Run(async () =>
+                    {
+                        return await mUser.DeleteUserList(UserSelect);
+                    });
+
+                    Message = kq.Message;
+
+                    if (kq.IsSuccess)
+                    {
+                        PerformLoadAll();
+                    }
+                }    
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+            finally
+            {
+                vmMainDc.IsWinActive = true;
+                vmMainDc.SbMessage?.Enqueue(Message, null, null, null, false, true, TimeSpan.FromSeconds(2));
+            }
+        }
+    }
+}
